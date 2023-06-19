e732660eb659c79c624daea0af6c77874d15b38b
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-06-19 22:21:41 +0600

    user is not created with simmiler user

--- services/user.services.js
+++ services/user.services.js
@@ -3,6 +3,21 @@ const User = require("../model/user")
 
 // save a user services ---------------------------
 exports.createAuserService = async(data)=>{
+
+if(data){
+    const userData = await User.findOne({email: {$eq: data.email}})
+    console.log("userData",userData)
+
+    if(userData){
+        const result = "User is already created";
+        return result
+    }
+}
+
+else{
     const result = await User.create(data)
     return result;
+}
+
+    
 }
