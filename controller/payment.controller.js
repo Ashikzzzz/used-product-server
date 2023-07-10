9efcab3fa06aaae63985734acbc14ea7fa2ad12a
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-07-10 18:14:22 +0600

    ssl continue

--- controller/payment.controller.js
+++ controller/payment.controller.js
@@ -1,6 +1,7 @@
 
 const SSLCommerzPayment = require('sslcommerz-lts')
 const { createAPaymentServices } = require("../services/payment.services");
+const Payment = require('../model/payment');
 
 
 // create a payment -------------------------------
@@ -8,7 +9,7 @@ exports.createAPayment = async (req, res) => {
 
     try {
         const paymentInfo = req.body;
-        // console.log("paymentInfo",paymentInfo)
+        console.log("paymentInfo",paymentInfo)
         const id = paymentInfo.id
         const findBookingInfo = await createAPaymentServices(id)
         console.log("findBookingInfo",findBookingInfo)
@@ -19,10 +20,10 @@ exports.createAPayment = async (req, res) => {
             total_amount: findBookingInfo?.reslae_price,
             currency: paymentInfo.currency,
             tran_id: transactionId, // use unique tran_id for each api call
-            success_url: 'http://localhost:3030/success',
-            fail_url: 'http://localhost:3030/fail',
-            cancel_url: 'http://localhost:3030/cancel',
-            ipn_url: 'http://localhost:3030/ipn',
+            success_url: `${process.env.ROOT}/ssl-payment-success?transactionId=${transactionId}`,
+            fail_url:  `${process.env.ROOT}/ssl-payment-failure?transactionId=${transactionId}`,
+            cancel_url:  `${process.env.ROOT}/ssl-payment-cancel`,
+            ipn_url: `${process.env.ROOT}/ssl-payment-ipn`,
             shipping_method: 'Courier',
             product_name: paymentInfo.name,
             product_category: paymentInfo. category_name,
@@ -52,7 +53,14 @@ exports.createAPayment = async (req, res) => {
         sslcz.init(data).then(async apiResponse => {
             // Redirect the user to payment gateway
             let GatewayPageURL = apiResponse.GatewayPageURL
-            console.log("GatewayPageURL",GatewayPageURL)
+            // console.log("GatewayPageURL",GatewayPageURL)
+
+              const paymentData=    await Payment.create({
+                ...paymentInfo,
+                transactionId,
+                paid: false,
+            });
+            console.log("paymentData",paymentData)
             res.send( {url : GatewayPageURL})
             console.log('Redirecting to: ', GatewayPageURL)
         
@@ -61,6 +69,20 @@ exports.createAPayment = async (req, res) => {
 
     }
     catch (error) {
+        res.status(400).json({
+            status: 'error',
+            massage: "Data inserted Error",
+            error: error.message
+        })
+    }
+}
+
+// success payment controller 
+exports.successPayment = async(req, res)=>{
+    try {
+        console.log("success")
+    }
+     catch (error) {
         res.status(400).json({
             status: 'error',
             massage: "Data inserted Error",
--- routes/payment.route.js
+++ routes/payment.route.js
@@ -5,5 +5,10 @@ const paymentController= require("../controller/payment.controller")
 router.route("/ssl-request")
     .post(paymentController.createAPayment)
 
+    // success payment route -----------
+
+    router.route("/ssl-request-success")
+    .post(paymentController.successPayment)
+
 
 module.exports = router;
